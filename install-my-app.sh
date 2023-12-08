662d57870538a3dfa9eeb9856699e2bc8d9cb0b4
jackie <lianghengwei2011@163.com>
2023-12-08 20:33:47 +0800

    Add script to install my app

---
+++ install-my-app.sh
@@ -0,0 +1,6 @@
+# install git in debian
+sudo apt-get install git
+
+git clone -b master https://github.com/HEI201/Trojan-fork.git
+
+cd Trojan-fork && chmod +x trojan_install.sh && ./trojan_install.sh gghh@rr.com love2.vip
